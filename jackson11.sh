e89de7382fe8c74c88e7c2985d6e845ddc7ce8b2
jacksonfernando <jacksonfernando51@gmail.com>
2018-10-21 04:52:15 -0800

    aa

--- jackson11.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash chat_id="487592203"
-token="528054963:AAFts4tGKuPXYBBSUCkFSRs_qEpBHfp-l1U"
-msg="vv" curl -s -F chat_id=487592203 -F text="Coba dengan Bash cuy" https://api.telegram.org/bot528054963:AAFts4tGKuPXYBBSUCkFSRs_qEpBHfp-l1U/sendMessage > /dev/null
