f8236713487664e5854ee3082472207b8c93d814
jacksonfernando <jackson@pop-os.localdomain>
2019-10-06 00:26:55 +0700

    adding shell.py file

--- main.py
+++ main.py
@@ -28,46 +28,38 @@ class ServerBot(telepot.Bot):
 
     def on_chat_message(self , msg):
         content_type, chat_type, chat_id = telepot.glance(msg)
-        print("Chat id admin :" +str(chat_id))
+        print("User chat id :" +str(chat_id))
         if chat_id in adminchatid:
             performanceResult = monitorPerformance(chat_id)
             if content_type == 'text':
-                if msg['text'] == '/stats' and chat_id not in shellExecution:
+                if msg['text'] == '/status' and chat_id not in shellExecution:
                     bot.sendMessage(chat_id, performanceResult, disable_web_page_preview=True)
-                elif msg['text'] == 'Stop':
+                elif msg['text'] == 'stop':
                     clearAll(chat_id)
-                    bot.sendMessage(chat_id, "hentikan semua operasi" ,  reply_markup=hide_keyboard)
+                    bot.sendMessage(chat_id, "Stop all operation" ,  reply_markup=hide_keyboard)
                 elif msg['text'] == '/shell' and chat_id  not in shellExecution:
-                    bot.sendMessage(chat_id, "masukan perintah atau command " , reply_markup=stopmarkup)
+                    bot.sendMessage(chat_id, "Enter shell command" , reply_markup=stopmarkup)
                     shellExecution.append(chat_id)
-                elif msg['text'] == '/changeDirectory ':
-                    bot.sendMessage(chat_id,"masukan nama direktori" , reply_markup=stopmarkup)
+                elif msg['text'] == '/chdir':
+                    bot.sendMessage(chat_id,"Enter direcotry name " , reply_markup=stopmarkup)
                     changeDirectory .append(chat_id)
                 elif chat_id in changeDirectory :
                     p = str(msg['text'])
                     if os.path.isdir(p) == 1:
                         os.chdir (p)
-                        bot.sendMessage(chat_id,"direktori telah diganti ke"+p ,reply_markup=hide_keyboard)
+                        bot.sendMessage(chat_id,"Directory is change into "+p ,reply_markup=hide_keyboard)
                         clearAll(chat_id)
                     else:
-                        bot.sendMessage(chat_id,"tidak ada direktori")
+                        bot.sendMessage(chat_id,"Dir is not available")
                 elif chat_id in shellExecution:
-                    p = Popen(msg['text'] , shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+                    p = Popen(msg['text'].lower() , shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
                     output = p.stdout.read()
                     if output != b'':
                         bot.sendMessage(chat_id, output, disable_web_page_preview=True)
                     else:
                         bot.sendMessage(chat_id, "No output.", disable_web_page_preview=True)
-                elif msg['text'] == '/reboot':
-                    p = subprocess.Popen(['reboot'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-                    out,err = p.communicate()
-                    bot.sendMessage(chat_id,"server akan di reboot")
-                elif msg['text'] == '/shutdown':
-                    p = subprocess.Popen(['shutdown','now'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-                    matikan = p.communicate()
-                    bot.sendMessage(chat_id,"server akan segera dimatikan")
         else:
-            bot.sendMessage(chat_id, "user ini bukan admin")
+            bot.sendMessage(chat_id, "This user dont have the permission")
 
 
 
